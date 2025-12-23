bfd0affe37a681e634b0bd858c55322fc7579809
gahui <jess5858@naver.com>
2025-12-23 09:44:42 +0900

    Build: Docker 관련 인프라 수정

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,9 +1,16 @@
 import os
 from datetime import timedelta
 from pathlib import Path
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+GOOGLE_MAPS_API_KEY = env('GOOGLE_MAPS_API_KEY')
 
 # Quick-start development settings - unsuitable for production
 # SECURITY WARNING: keep the secret key used in production secret!
